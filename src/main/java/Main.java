f2d901f0dc6d3f8e7c88cfce6e6be1f435128083
Kwizard <serik.makzhanov@gmail.com>
2023-04-19 23:45:24 +0600

    added readString()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        readFile();
+        readString();
+    }
+
+    static void readString(){
+        File inputText = new File("./test.txt");
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(inputText);
+        } catch (FileNotFoundException e) {
+            System.out.println("Не удалось найти файл");
+        }
+        StringBuilder inputString = new StringBuilder();
+        while (scanner.hasNextLine()) {
+            inputString.append(scanner.nextLine().replace("\n", ""));
+        }
+        scanner.close();
     }
 }
